ed495fcdc9d5c57ae23ad0d2a20a0916405d8759
fatahillah22 <fatah.hanif@gmail.com>
2015-09-27 23:49:23 +0700

    iseng om

--- index.php
+++ index.php
@@ -1,1 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+}
+
+h1 {
+    font-family: "Alien League";
+    color: orange;
+    text-align: center;
+}
+
+p {
+    font-family: "Alien League";
+    font-size: 50px;
+    color: White;
+    text-align: center;
+}
+body {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+    -webkit-animation-name: example; /* Chrome, Safari, Opera */
+    -webkit-animation-duration: 4s; /* Chrome, Safari, Opera */
+    animation-name: example;
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+}
+
+/* Chrome, Safari, Opera */
+@-webkit-keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    25%  {background-color:yellow; left:200px; top:0px;}
+    50%  {background-color:blue; left:200px; top:200px;}
+    75%  {background-color:green; left:0px; top:200px;}
+    100% {background-color:red; left:0px; top:0px;}
+}
+
+/* Standard syntax */
+@keyframes example {
+    0%   {background-color:red; left:0px; top:0px;}
+    25%  {background-color:yellow; left:200px; top:0px;}
+    50%  {background-color:blue; left:200px; top:200px;}
+    75%  {background-color:green; left:0px; top:200px;}
+    100% {background-color:red; left:0px; top:0px;}
+}
+</style>
+</head>
+<body>
+
 <h1>Hai Kepoers :3</h1>
+<p>Ini namanya template :3</p>
+</body>
+</html>
